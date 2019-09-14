5a4006b78b3bb48bb715b1f0ed99ab4ac9bbfb35
Naveed Ganjani <nganjani@protonmail.com>
2019-09-14 18:54:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,10 +2,6 @@ require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-  emoticons.each do |emoticon|
-    get_meaning[emoticon] = {}
-  end
-  emoticons
 end
 
 def get_japanese_emoticon
