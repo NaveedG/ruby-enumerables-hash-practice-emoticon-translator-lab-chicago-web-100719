6a3e1df3f2f19b7438d2ddd550bb1639bb0dc478
Naveed Ganjani <nganjani@protonmail.com>
2019-09-14 19:56:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
     if !new_hash[:get_meaning]
       new_hash[:get_meaning] = { emo_pair[1] => nil }
     else
-      new_hash[:get_meaning][emo_pair[1] => nil ]
+      new_hash[:get_meaning][emo_pair[1]] = nil
     end
 
     if !new_hash[:get_emoticon]
