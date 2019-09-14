71eeeeabbc5903157ef391528265bcd73e9cfae9
Naveed Ganjani <nganjani@protonmail.com>
2019-09-14 19:34:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_path)
   emoticons.each do |emoticon|
     emoticon.each do |eng_meaning, emoticon_pair|
     new_hash[:get_meaning][:japanese] = [] if !new_hash[:get_meaning][:japanese]
-    new_hash[:get_meaning][:japanese] << emoticon_pair[1]
+    new_hash[:get_meaning][:japanese] << emoticon_pair
   end
   end
   new_hash
