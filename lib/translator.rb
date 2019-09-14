345e711a69c080ee40d466ebc195c9e7a82eb683
Naveed Ganjani <nganjani@protonmail.com>
2019-09-14 19:26:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticons.each do |eng_meaning, emoticon_pair|
     new_hash[:get_meaning][:japanese] = []
-    new_hash[:get_meaning][:japanese].push(emoticon_pair(1))
+    new_hash[:get_meaning][:japanese] << emoticon_pair(1)
   end
   new_hash
 end
